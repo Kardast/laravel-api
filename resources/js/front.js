c0e6ebffbe83e19c29dd3f3b5fe8d2ad02f5e9e4
Kardast <dottorsf@gmail.com>
2022-07-28 16:26:10 +0200

    first commit

---
+++ resources/js/front.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+
+window.Vue = require('vue');            // importiamo la libreria Vue
+import App from './components/App.vue'; // importiamo il componente base App.vue e lo assegniamo alla variabile App
+
+const app = new Vue({
+    el: '#root',                        // id del componente nel file HTML dentro il quale opererà Vue
+    render: h => h(App)                 // monta il componente App nell'elemento root
+});
